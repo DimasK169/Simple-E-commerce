2c22b4cfe14953a98e0dd22977032198506c60e0
Dimas Kenang Surya Ardani <dimasardani66@gmail.com>
2025-04-14 15:52:53 +0700

    Revert "Revert 2 main"

---
+++ flash-sale-service/src/main/java/com/flash_sale/app/FlashSaleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flash_sale.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashSaleServiceApplication.class, args);
+	}
+
+}
--- product-service/src/main/java/com/product/app/service/interfacing/AuditTrailsService.java
+++
@@ -1,9 +0,0 @@
-package com.product.app.service.interfacing;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.product.app.dto.request.AuditTrailsRequest;
-import com.product.app.dto.result.AuditTrailsResponse;
-
-public interface AuditTrailsService {
-    AuditTrailsResponse saveAuditTrails(AuditTrailsRequest request) throws JsonProcessingException;
-}
