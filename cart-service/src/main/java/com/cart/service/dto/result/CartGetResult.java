95833d9a8cdd61efe82489397937a7a498084999
Anugrah <anugrahnunu858@gmail.com>
2025-04-18 18:40:57 +0700

    update: payment and cart

--- cart-service/src/main/java/com/cart/service/dto/result/CartGetResult.java
+++ cart-service/src/main/java/com/cart/service/dto/result/CartGetResult.java
@@ -1,5 +1,6 @@
 package com.cart.service.dto.result;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
@@ -11,26 +12,42 @@ import java.util.Date;
 public class CartGetResult {
 
     @JsonProperty("Product_Name")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private String productName;
 
     @JsonProperty("Product_Desc")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private String productDesc;
 
     @JsonProperty("Product_Price")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Integer productPrice;
 
+    @JsonProperty("Product_Code")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String productCode;
+
+    @JsonProperty("Fs_Code")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String fsCode;
+
     @JsonProperty("Cart_Total_Price_Per_Item")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Integer cartTotalPricePerItem;
 
     @JsonProperty("Cart_Total_Price")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Integer cartTotalPrice;
 
     @JsonProperty("Cart_Quantity")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Integer cartQuantity;
 
     @JsonProperty("Product_Image")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private String productImage;
 
     @JsonProperty("Created_Date")
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Date createdDate;
 }
