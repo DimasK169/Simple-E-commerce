981b883731efc793672b0f150e89fcf445eb3d15
Ariz-99 <arizmuhammadfajar@gmail.com>
2025-04-21 22:56:58 +0700

    fix start date & end date

--- flash-sale-service/src/main/java/com/flash_sale/app/dto/request/FlashSaleUpdateRequest.java
+++ flash-sale-service/src/main/java/com/flash_sale/app/dto/request/FlashSaleUpdateRequest.java
@@ -25,11 +25,11 @@ public class FlashSaleUpdateRequest {
 
     @JsonProperty("FlashSale_StartDate")
     @NotNull(message = "Flash sale start date cannot be empty")
-    private Date fsStartDate;
+    private LocalDateTime fsStartDate;
 
     @JsonProperty("FlashSale_EndDate")
     @NotNull(message = "Flash sale end date cannot be empty")
-    private Date  fsEndDate;
+    private LocalDateTime  fsEndDate;
 
     @JsonProperty("FlashSale_Discount")
     @NotNull(message = "Flash sale discount cannot be empty")
